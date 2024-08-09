e9416844308531c1007afe5a3f4a066ff7e68874
riidans <109998029+riidans@users.noreply.github.com>
2024-08-09 05:42:00 -0400

    Refactor code & implement keyboard input to calculator project.

--- calculator/main.js
+++ calculator/main.js
@@ -1,3 +1,6 @@
+const operatorSymbols = {"":"", "add": "+", "subtract": "-", "multiply": "×", "divide": "÷"}
+
+// Basic calculation functions
 function add(a, b) {
     return a + b
 }
@@ -14,6 +17,7 @@ function divide(a, b) {
     return a / b 
 }
 
+// Handler for basic calculation functions
 function operate(a, b, operator) {
     if (operator == "add") {
         return add(a, b)
@@ -29,83 +33,152 @@ function operate(a, b, operator) {
     }
 }
 
-function handleInputs(value, text, isOperator) {
-    // Handles inputs and assigns global variables
+function handleInputs(value, isOperator) {
+    // THIS IS THE ONLY FUNCTION THAT HANDLES GLOBAL VARIABLES
+    // Variables are filled in the order of: num1 --> operator --> num2
 
-    // Operator functions
+    // Handles operator functions
     if (isOperator) {
         if (value === "clear") {
-            num1 = num2 = operator = undefined
-            handleDisplay("0", false)
+            num1 = num2 = operator = ""
         }
-        else if (num1 === undefined) {
-            return
-        }
-        else if (num1 && num2 && operator && value) {
-
-            if (num2 == 0 && operator === "divide") {
-                alert("Cannot divide by 0!")
-                return
-            }
-            num1 = (+operate(parseFloat(num1), parseFloat(num2), operator).toFixed(5)).toString()
-            num2 = operator = undefined
-            handleDisplay(num1, false)
-
-            if (value != "equals") {
-                operator = value
-                handleDisplay(text, true)
-            }
-        }
-        else if (num1 && value === "equals") {
-            return
+        else if (value === "delete") {
+            [num1, num2, operator] = deleteInput(num1, num2, operator) 
         }
         else {
-            operator = value
-            handleDisplay(text, true)
+            [num1, num2, operator] = isOperation(num1, num2, operator, value)
         }
     }
 
-    // Number functions
+    // Handles number functions
     else {
-        if (operator === undefined) {
-            if (num1 === undefined && value === ".") { num1 = "0" }
-            else if (num1 === undefined) { num1 = "" }
-            else if (num1.includes(".") && value === ".") { return }
-            num1 += value
-            handleDisplay(num1, false)
-        }
-        else {
-            if (num2 === undefined && value === ".") { num2 = "0" }
-            else if (num2 === undefined) { num2 = "" }
-            else if (num2.includes(".") && value === ".") { return }
-            num2 += value
-            handleDisplay(num2, false)
-        }
+        [num1, num2] = storeOperands(num1, num2, operator, value)
+    }
+    
+    // Update display
+    handleDisplay(num1, num2, operator)
+}
+
+function handleDisplay(num1, num2, operator) {
+    // Defaults to 0 if there is no input
+    if (num1 === "") {
+        display.textContent = 0
+    }
+    else {
+        display.textContent = `${num1} ${operatorSymbols[operator]} ${num2}`
+    }
+}   
+
+function deleteInput(num1, num2, operator) {
+    // Variables are filled in the order of: num1 --> operator --> num2 
+    // If num1 is empty, do not do anything
+    if (num1 === "") {
+        return [num1, num2, operator]
+    }
+    // If operator is empty, delete from num1
+    else if (operator === "") {
+        return [num1.slice(0, -1), num2, operator]
+    }
+    // If num2 is empty and there is an operator, delete from operator
+    else if (num2 === "" && operator.length != 0) {
+        return [num1, num2, ""]
+    }
+    // Delete from num2
+    else {
+        return [num1, num2.slice(0, -1), operator]
     }
 }
 
-function handleDisplay(value, isOperator) {
-    if (isOperator) {
-        display.textContent += value
+function storeOperator(num1, operator, value) {
+    // If num1 is empty, do not do anything
+    if (num1 === "") {
+        return operator
     }
-    else {
-        display.textContent = value
-    }
+    return value
 }
 
-let num1, num2, operator
-num1 = num2 = operator = undefined
+function storeOperands(num1, num2, operator, value) {
+    // Stores to num1 since operator does not exist
+    if (operator === "") {
+        if (value === ".") {
+            value = isValidDecimal(num1)
+        }
+        num1 += value
+    }
+
+    // Stores to num2 since operator exists
+    else {
+        if (value === ".") {
+            value = isValidDecimal(num2)
+        }
+        num2 += value
+    }
+    return [num1, num2]
+}
+
+function isValidDecimal(num) {
+    // Decimal in the start
+    if (num === "") {
+        return "0."
+    }
+    // Invalid decimal
+    else if (num.includes(".")) {
+        return "" 
+    }
+    // Decimal in the middle
+    else {
+        return "."
+    }
+    
+}
+
+function isOperation(num1, num2, operator, value) {    
+    // Store operator if no operator exists
+    if (num1 && !operator && !num2 && (value != "equals")) {
+        operator = value
+    }
+    
+    // Divide by 0 error
+    else if ((num2 == 0) && (operator === "divide")) {
+        alert("Cannot divide by 0!")
+    }
+
+    // Otherwise, complete the operation
+    else {
+        num1 = (+operate(parseFloat(num1), parseFloat(num2), operator)
+                                            .toFixed(5))
+                                            .toString()
+        num2 = ""
+        if (value == "equals") { operator = "" }
+        else { operator = value }
+    }
+    return [num1, num2, operator]
+}
+
+let num1, num2, operator, expression
+num1 = num2 = operator = ""
 
 const display = document.querySelector(".display")
 
 const numberButtons = document.querySelectorAll(".num")
 numberButtons.forEach((button) => {
-    button.addEventListener('click', () => {handleInputs(button.textContent, button.textContent, false)})
+    button.addEventListener('click', () => {handleInputs(button.textContent, false)})
 })
 
 const operatorButtons = document.querySelectorAll(".op") 
 operatorButtons.forEach((button) => {
-    button.addEventListener('click', () => {handleInputs(button.id, button.textContent, true)})
+    button.addEventListener('click', () => {handleInputs(button.id, true)})
 })
 
+function convertKeyboardInput(e) {
+    if(e.key >= 0 && e.key <= 9) { handleInputs(e.key, false) }
+    else if(e.key === ".") { handleInputs(".", false) }
+    else if(e.key === "+") { handleInputs("add", true) }
+    else if(e.key === "-") { handleInputs("subtract", true) }
+    else if(e.key === "*") { handleInputs("multiply", true) }
+    else if(e.key === "/") { handleInputs("divide", true) }
+    else if(e.key === "=" || e.key === "Enter") { handleInputs("equals", true) }
+    else if(e.key === "Backspace") { handleInputs("delete", true) }
+}
 
+document.addEventListener('keydown', convertKeyboardInput)
